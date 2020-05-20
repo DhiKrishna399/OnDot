2e8e15593cf0d9343add970950ccd9601eb013a9
connarwang1987 <lwang130@ucsc.edu>
2020-05-20 14:18:39 -0700

    fuck formatting

--- project_q/lib/main.dart
+++ project_q/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:project_q/screens/maps_page.dart';
+import 'package:provider/provider.dart';
 
+import 'providers/auth.dart';
+import 'models/http_exception.dart';
 import './screens/main_user_info.dart';
 
 void main() => runApp(MyApp());
@@ -8,10 +11,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "ImGame",
-      home: ImGame(),
-    );
+     return MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(
+            value: Auth(),
+          )
+        ],
+        child: MaterialApp(
+          title: 'IMGAME',
+          
+          home: ImGame(),
+        ));
+  
   }
 }
 
--- project_q/lib/screens/login_page.dart
+++ project_q/lib/screens/login_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'maps_page.dart';
 
-class LoginPage extends StatelessWidget {
+import '../providers/auth.dart';
+import '../models/http_exception.dart';
+
+
+
+class LoginPage extends StatefulWidget {
   const LoginPage({
     Key key,
     @required this.queryData,
@@ -14,6 +20,81 @@ class LoginPage extends StatelessWidget {
   final Function mapsPageRoute;
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+  var _isLoading = false;
+  final _passwordController = TextEditingController();
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('An Error Occurred!'),
+        content: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Okay'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> _submit() async {
+    if (!_formKey.currentState.validate()) {
+      // Invalid!
+      return;
+    }
+    _formKey.currentState.save();
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      // Log user in
+      await Provider.of<Auth>(context, listen: false).login(
+        _authData['email'],
+        _authData['password'],
+      );
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MapsPage()),
+      );
+    } on HttpException catch (error) {
+      var errorMessage = 'Authentication failed';
+      if (error.toString().contains('EMAIL_EXISTS')) {
+        errorMessage = 'This email address is already in use.';
+      } else if (error.toString().contains('INVALID_EMAIL')) {
+        errorMessage = 'This is not a valid email address';
+      } else if (error.toString().contains('WEAK_PASSWORD')) {
+        errorMessage = 'This password is too weak.';
+      } else if (error.toString().contains('EMAIL_NOT_FOUND')) {
+        errorMessage = 'Could not find a user with that email.';
+      } else if (error.toString().contains('INVALID_PASSWORD')) {
+        errorMessage = 'Invalid password.';
+      }
+      _showErrorDialog(errorMessage);
+    } catch (error) {
+      const errorMessage =
+          'Could not authenticate you. Please try again later.';
+      _showErrorDialog(errorMessage);
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -24,53 +105,72 @@ class LoginPage extends StatelessWidget {
           textAlign: TextAlign.center,
         ),
         const SizedBox(height: 40),
-        Container(
-          //Email Field
-          height: 50,
-          width: queryData.size.width / 1.5,
-          child: TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            textAlign: TextAlign.center,
-            decoration: InputDecoration(
-              border: new OutlineInputBorder(
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(10),
+         Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 50,
+                  width: widget.queryData.size.width / 1.5,
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                        Radius.circular(10),
+                        ),
+                      ),
+                      hintText: "Enter Email",
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Invalid email!';
+                      }
+                    },
+                    onSaved: (value) {
+                      _authData['email'] = value;
+                    },
+                  ),
                 ),
-              ),
-              hintText: "Enter email",
-            ),
-          ),
-        ),
-        SizedBox(height: 15),
-        Container(
-          //Password Field
-          height: 50,
-          width: queryData.size.width / 1.5,
-          child: TextFormField(
-            obscureText: true,
-            textAlign: TextAlign.center,
-            decoration: InputDecoration(
-              border: new OutlineInputBorder(
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(10),
+
+                SizedBox(height: 10),
+                Container(
+
+                  height: 50,
+                  width: widget.queryData.size.width / 1.5,
+                  child: TextFormField(
+                   obscureText: true,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      hintText: "Password",
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 5) {
+                        return 'Password is too short!';
+                      }
+                    },
+                    onSaved: (value) {
+                      _authData['password'] = value;
+                    },
+                  ),
                 ),
+              ],
               ),
-              hintText: "Password",
-            ),
           ),
-        ),
+          ),
         SizedBox(height: 5),
         Container(
-          width: queryData.size.width / 2,
+          width: widget.queryData.size.width / 2,
           child: RaisedButton(
             
-            onPressed: () {
-              //  Navigator.push(
-              //    context,
-              //    MaterialPageRoute(builder: (context) => MapsPage()),
-              //  );
-              mapsPageRoute();
-            },
+            onPressed: _submit,
             shape: new RoundedRectangleBorder(
               borderRadius: new BorderRadius.circular(10),
             ),
@@ -85,7 +185,7 @@ class LoginPage extends StatelessWidget {
         InkWell(
           child: new Text('New user? Sign up here'),
           onTap: () {
-            selectorHandler();
+            widget.selectorHandler();
           },
         ),
       ],
