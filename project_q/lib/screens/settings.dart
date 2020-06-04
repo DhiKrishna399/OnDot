b1b2eba16e905bc78ea3d3f5dc33fda66445f22c
Dhiva Krishna <dhivakrishna@gmail.com>
2020-06-03 21:32:55 -0700

    debug pushing sign out down

--- project_q/lib/screens/settings.dart
+++ project_q/lib/screens/settings.dart
@@ -15,106 +15,113 @@ class _SettingsPageState extends State<SettingsPage> {
     SizeConfig().init(context);
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            SettingsHeader(),
-            SizedBox(
-              height: SizeConfig.blockSizeVertical * 6,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 30, right: 25),
-              child: Column(
-                //crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    //Row tht holds 'Account' and edit button
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Account',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            fontSize: SizeConfig.blockSizeHorizontal * 6,
-                            color: Colors.black),
-                      ),
-                      Container(
-                        //color: Colors.black,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25),
-                          color: Colors.black,
-                        ),
-
-                        child: IconButton(
-                            icon: Icon(
-                              Icons.edit,
-                              color: Colors.white,
-                            ),
-                            onPressed: null),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: SizeConfig.blockSizeVertical * 2,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Name: Buzz',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(
-                        height: SizeConfig.blockSizeVertical * 1.5,
-                      ),
-                      Text(
-                        'Email: ImGame@ImGame.com',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(
-                        height: SizeConfig.blockSizeVertical * 3,
-                      ),
-                      Divider(
-                        color: Colors.black,
-                      ),
-                      SizedBox(
-                        height: SizeConfig.blockSizeVertical * 5,
-                      ),
-                      InkWell(
-                        child: new Text('Notifications',
-                            style: TextStyle(fontSize: 24)),
-                        onTap: () {},
-                      ),
-                      SizedBox(
-                        height: SizeConfig.blockSizeVertical * 5,
-                      ),
-                      InkWell(
-                        child: new Text('Map', style: TextStyle(fontSize: 24)),
-                        onTap: () {},
-                      ),
-                      SizedBox(
-                        height: SizeConfig.blockSizeVertical * 5,
-                      ),
-                      InkWell(
-                        child: new Text('Activities',
-                            style: TextStyle(fontSize: 24)),
-                        onTap: () {},
-                      ),
-                    ],
-                  ),
-                ],
+        child: Container(
+          color: Colors.red[50],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              SettingsHeader(),
+              SizedBox(
+                height: SizeConfig.blockSizeVertical * 6,
               ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                InkWell(
-                  child: new Text('Sign Out', style: TextStyle(fontSize: 20)),
-                  onTap: () {},
+              Padding(
+                padding: EdgeInsets.only(left: 30, right: 25),
+                child: Container(
+                  color: Colors.green[100],
+                  child: Column(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        //Row tht holds 'Account' and edit button
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Account',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                fontSize: SizeConfig.blockSizeHorizontal * 6,
+                                color: Colors.black),
+                          ),
+                          Container(
+                            //color: Colors.black,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              color: Colors.black,
+                            ),
+
+                            child: IconButton(
+                                icon: Icon(
+                                  Icons.edit,
+                                  color: Colors.white,
+                                ),
+                                onPressed: null),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: SizeConfig.blockSizeVertical * 2,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Name: Buzz',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: SizeConfig.blockSizeVertical * 1.5,
+                          ),
+                          Text(
+                            'Email: ImGame@ImGame.com',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: SizeConfig.blockSizeVertical * 3,
+                          ),
+                          Divider(
+                            color: Colors.black,
+                          ),
+                          SizedBox(
+                            height: SizeConfig.blockSizeVertical * 5,
+                          ),
+                          InkWell(
+                            child: new Text('Notifications',
+                                style: TextStyle(fontSize: 24)),
+                            onTap: () {},
+                          ),
+                          SizedBox(
+                            height: SizeConfig.blockSizeVertical * 5,
+                          ),
+                          InkWell(
+                            child: new Text('Map', style: TextStyle(fontSize: 24)),
+                            onTap: () {},
+                          ),
+                          SizedBox(
+                            height: SizeConfig.blockSizeVertical * 5,
+                          ),
+                          InkWell(
+                            child: new Text('Activities',
+                                style: TextStyle(fontSize: 24)),
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  color: Colors.red,
+                  child: InkWell(
+                    child: new Text('Sign Out', style: TextStyle(fontSize: 20)),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
