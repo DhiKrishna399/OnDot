0bc23e87d9954fce46fbaa32b7abb81acce9aa02
Luqman Abdurrohman <luggyman123@gmail.com>
2020-07-05 13:05:43 -0700

    animateCamera bug

--- project_q/lib/providers/maps_provider.dart
+++ project_q/lib/providers/maps_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:geolocator/geolocator.dart';
@@ -5,22 +7,23 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:project_q/models/event_dummy.dart';
 
 class MapsProvider extends ChangeNotifier {
-
   MapType currentMapType = MapType.normal;
   Set<Marker> eventMarkers = {};
   Position currentPosition;
   LatLng center;
+
   GoogleMapController mapsController;
-  
+
   bool isLoading = false;
 
-  void displayEventMarkers(){
+  void displayEventMarkers() {
     eventDummy.forEach((element) {
       eventMarkers.add(
         Marker(
           markerId: MarkerId(element.title),
           draggable: false,
-          infoWindow: InfoWindow(title: element.title, snippet: element.description),
+          infoWindow:
+              InfoWindow(title: element.title, snippet: element.description),
           position: element.locationCoords,
         ),
       );
@@ -32,11 +35,10 @@ class MapsProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void mapCreated(GoogleMapController mapsController){
-     mapsController = mapsController;
-     notifyListeners();
+  void mapCreated(GoogleMapController mapsController) {
+    mapsController = mapsController;
+    notifyListeners();
   }
-
 
   void onCameraMove(CameraPosition position) {
     center = position.target;
@@ -57,5 +59,35 @@ class MapsProvider extends ChangeNotifier {
     });
   }
 
+  // void moveCamera() {
+    
+  //   mapsController.animateCamera(
+  //     CameraUpdate.newCameraPosition(
+  //       CameraPosition(
+  //         target: LatLng(38.016536, -122.137974),
+  //         zoom: 14,
+  //         bearing: 45,
+  //         tilt: 45,
+  //       ),
+  //     ),
+  //   );
+  //   mapCreated(mapsController);
+  //   notifyListeners();
+  // }
 
+  // Future<void> animateTo() async {
+  //   final GoogleMapController c = await mapsController.future;
+
+  //   final p = CameraPosition(
+  //       target: LatLng(38.016536, -122.137974),
+  //       zoom: 14.4746);
+         
+  //   c.animateCamera(CameraUpdate.newCameraPosition(p));
+
+  //   notifyListeners();
+  // }
+
+  void test() {
+    print('hello');
+  }
 }
--- project_q/lib/widgets/home_screen_widgets/carousel_events.dart
+++ project_q/lib/widgets/home_screen_widgets/carousel_events.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:project_q/models/event_dummy.dart';
 import 'package:project_q/models/size_config.dart';
+import 'package:project_q/providers/maps_provider.dart';
 import 'package:project_q/widgets/card_widgets/event_card.dart';
+import 'package:provider/provider.dart';
 
 class EventCarousel extends StatefulWidget {
   const EventCarousel({
@@ -15,34 +18,37 @@ class EventCarousel extends StatefulWidget {
 }
 
 class _EventCarouselState extends State<EventCarousel> {
-  PageController _pageController;
+  PageController pageController;
   int prevPage;
 
   void initState() {
     super.initState();
 
-    _pageController = PageController(initialPage: 1, viewportFraction: 0.8)
+    pageController = PageController(initialPage: 1, viewportFraction: 0.8)
       ..addListener(_onScroll);
   }
 
   void _onScroll() {
-    if (_pageController.page.toInt() != prevPage) {
-      prevPage = _pageController.page.toInt();
-      //moveCamera();
+    if (pageController.page.toInt() != prevPage) {
+      prevPage = pageController.page.toInt();
+      //Provider.of<MapsProvider>(context, listen: false).test();
+      //Provider.of<MapsProvider>(context, listen: false).moveCamera(pageController);
+
     }
   }
 
   _eventCardList(int index) {
     return AnimatedBuilder(
-      animation: _pageController,
+      animation: pageController,
       builder: (BuildContext context, Widget widget) {
         double value = 1;
-        if (_pageController.position.haveDimensions) {
-          value = _pageController.page - index;
-          value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
+        if (pageController.position.haveDimensions) {
+          value = pageController.page - index;
+          value = (1 - (value.abs() * 0.34) + 0.06).clamp(0.0, 1.0);
         }
         return Center(
-          child: SizedBox(
+          child: Container(
+            margin: EdgeInsets.only(top: 100, left: 10, right: 10),            
             height: Curves.easeInOut.transform(value) * 125.0,
             width: Curves.easeInOut.transform(value) * 350.0,
             child: widget,
@@ -51,32 +57,37 @@ class _EventCarouselState extends State<EventCarousel> {
       },
       child: InkWell(
         onTap: () {
-          // moveCamera();
+          //Provider.of<MapsProvider>(context, listen: false).moveCamera();
+          //Provider.of<MapsProvider>(context, listen: false).moveCamera(pageController);
+          //print(pageController.page.toInt());
         },
-        child: Stack(
-          children: [
-            Center(
-              child: Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 10.0,
-                  vertical: 20.0,
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(7.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black54,
+                  offset: Offset(0.0, 4.0),
+                  blurRadius: 10.0,
                 ),
-                height: 125.0,
-                width: 275.0,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black54,
-                      offset: Offset(0.0, 4.0),
-                      blurRadius: 10.0,
-                    ),
-                  ],
+              ],
+            ),
+            child: Center(
+              child: Text(
+                eventDummy[index].title,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
                 ),
+                textAlign: TextAlign.center,
               ),
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
@@ -89,11 +100,11 @@ class _EventCarouselState extends State<EventCarousel> {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Container(
-        color: Colors.red,
+        //color: Colors.yellow[100],
         height: SizeConfig.screenHeight * .45,
         width: SizeConfig.screenWidth,
         child: PageView.builder(
-          controller: _pageController,
+          controller: pageController,
           itemCount: eventDummy.length,
           itemBuilder: (BuildContext context, int index) {
             return _eventCardList(index);
@@ -101,5 +112,9 @@ class _EventCarouselState extends State<EventCarousel> {
         ),
       ),
     );
+  }
+
+  void moveCamera(){
+    
   }
 }
